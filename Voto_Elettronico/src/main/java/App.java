1aa708ac2ec80be8ae73980e296bd7a287878714
gioripa <giorgio.ripamonti@studenti.unimi.it>
2022-02-15 10:06:52 +0100

    aggiunta vista grafica home

--- Voto_Elettronico/src/main/java/App.java
+++ Voto_Elettronico/src/main/java/App.java
@@ -1,9 +1,34 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class App {
+public class App extends Application {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+private  static Scene primaryScene;
 
-	}
+    public static void main(String[] args) {
+
+         launch();
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/home.fxml"));
+        Parent content = loader.load(); 
+
+        Scene main = new Scene(content);
+        primaryScene = main;
+        primaryStage.setScene(main);
+        primaryStage.show();
+
+    }
+
+    public static Scene getAppScene() {
+        return primaryScene;
+    }
 
 }
---
+++ Voto_Elettronico/src/main/java/HomeController.java
@@ -0,0 +1,22 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class HomeController {
+
+    @FXML
+    private Button btn;
+
+    @FXML
+    private TextField codiceFiscale;
+
+    @FXML
+    private PasswordField psw;
+
+    @FXML
+    void login(ActionEvent event) {
+    }
+
+}
