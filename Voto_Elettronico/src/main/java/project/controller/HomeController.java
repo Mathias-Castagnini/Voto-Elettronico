943cbbc4f2a43d7cc4031491eafe797e8c33a81d
gioripa <giorgio.ripamonti@studenti.unimi.it>
2022-02-24 10:04:11 +0100

    aggiunta controllo lunghezza psw e cf

--- Voto_Elettronico/src/main/java/project/controller/HomeController.java
+++ Voto_Elettronico/src/main/java/project/controller/HomeController.java
@@ -1,6 +1,8 @@
 package project.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -18,6 +20,12 @@ public class HomeController extends Controller{
 
     @FXML
     void login(ActionEvent event) {
+    	String cf = codiceFiscale.getText();
+    	String ps = psw.getText();
+    	if( cf.isEmpty() || ps.isEmpty()) {
+    		Alert l = new Alert(AlertType.ERROR,"Password e o Codice fiscale errato");
+    		l.showAndWait();
+    	}
     	//changeView("/view/",parameters); per passare alla seconda pagina
     }
 
