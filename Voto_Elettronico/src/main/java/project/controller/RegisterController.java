4215e7a663a9e1521c13156b32d0877c0e40c0ed
gioripa <giorgio.ripamonti@studenti.unimi.it>
2022-02-25 15:15:49 +0100

    aggiunta view di registrazione

---
+++ Voto_Elettronico/src/main/java/project/controller/RegisterController.java
@@ -0,0 +1,34 @@
+package project.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class RegisterController extends Controller{
+
+    @FXML
+    private Button btn;
+
+    @FXML
+    private TextField codiceFiscale;
+
+    @FXML
+    private PasswordField cpsw;
+
+    @FXML
+    private PasswordField psw;
+
+    @FXML
+    void login(ActionEvent event) {
+
+    }
+
+	@Override
+	public void init(Object parameters) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
