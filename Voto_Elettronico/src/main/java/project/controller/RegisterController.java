3bb67880997bf4017d3d19b9bbd3006a92d8524e
gioripa <giorgio.ripamonti@studenti.unimi.it>
2022-02-25 16:11:51 +0100

    modifiche vista

--- Voto_Elettronico/src/main/java/project/controller/RegisterController.java
+++ Voto_Elettronico/src/main/java/project/controller/RegisterController.java
@@ -8,6 +8,9 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class RegisterController extends Controller{
+	
+	@FXML
+    private Button back;
 
     @FXML
     private Button btn;
