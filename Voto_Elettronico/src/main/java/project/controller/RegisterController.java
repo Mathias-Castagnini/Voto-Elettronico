cab47c6feb92478cf5e71408b797adaa0e57b5f6
gioripa <giorgio.ripamonti@studenti.unimi.it>
2022-02-25 15:33:48 +0100

    modifica vista register

--- Voto_Elettronico/src/main/java/project/controller/RegisterController.java
+++ Voto_Elettronico/src/main/java/project/controller/RegisterController.java
@@ -3,6 +3,7 @@ package project.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
@@ -20,6 +21,9 @@ public class RegisterController extends Controller{
     @FXML
     private PasswordField psw;
 
+    @FXML
+    private CheckBox scr;
+    
     @FXML
     void login(ActionEvent event) {
 
