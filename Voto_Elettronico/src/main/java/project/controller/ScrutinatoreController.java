7925ca6af2e8c368377c10aee3cf4ffc60e3c8d1
gioripa <giorgio.ripamonti@studenti.unimi.it>
2022-02-26 16:49:58 +0100

    aggiunta vista scrutinatore

---
+++ Voto_Elettronico/src/main/java/project/controller/ScrutinatoreController.java
@@ -0,0 +1,47 @@
+package project.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ScrutinatoreController extends Controller{
+
+    @FXML
+    private Button bck;
+
+    @FXML
+    private Button cnd;
+
+    @FXML
+    private Button sess;
+
+    @FXML
+    private Button usr;
+
+    @FXML
+    void back(ActionEvent event) {
+    	changeView("/view/home.fxml",null);
+    }
+
+    @FXML
+    void candidate(ActionEvent event) {
+    	//changeView("/view/candidato.fxml",null);
+    }
+
+    @FXML
+    void session(ActionEvent event) {
+    	//changeView("/view/session.fxml",null);
+    }
+
+    @FXML
+    void user(ActionEvent event) {
+    	//changeView("/view/utente.fxml",null);
+    }
+
+	@Override
+	public void init(Object parameters) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
