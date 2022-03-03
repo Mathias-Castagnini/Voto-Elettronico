c9e70b89d7ff7b4a8906b9ce51908c248218b1c1
gioripa <giorgio.ripamonti@studenti.unimi.it>
2022-03-03 16:16:14 +0100

    modifica viste

--- Voto_Elettronico/src/main/java/project/controller/CreaSessioneController.java
+++ Voto_Elettronico/src/main/java/project/controller/CreaSessioneController.java
@@ -4,39 +4,62 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
 public class CreaSessioneController extends Controller{
 
-    @FXML
-    private Button bck;
+	 @FXML
+	    private Button bck;
 
-    @FXML
-    private Button crea;
+	    @FXML
+	    private Button crea;
 
-    @FXML
-    private TextField domanda;
+	    @FXML
+	    private TextField domanda;
 
-    @FXML
-    private ComboBox<?> mvittoria;
+	    @FXML
+	    private Button ins;
 
-    @FXML
-    private ComboBox<?> mvoto;
+	    @FXML
+	    private ListView<?> listPartiti;
 
-    @FXML
-    void back(ActionEvent event) {
-    	changeView("/view/gestioneSessione.fxml",null);
-    }
+	    @FXML
+	    private ListView<?> listPartitiInseriti;
 
-    @FXML
-    void creaSessione(ActionEvent event) {
+	    @FXML
+	    private ComboBox<?> mvittoria;
 
-    }
+	    @FXML
+	    private ComboBox<?> mvoto;
 
-	@Override
-	public void init(Object parameters) {
-		// TODO Auto-generated method stub
-		
-	}
+	    @FXML
+	    private Button tg;
+
+	    @FXML
+	    void back(ActionEvent event) {
+	    	changeView("/view/gestioneSessione.fxml",null);
+	    }
+
+	    @FXML
+	    void creaSessione(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void inserisci(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void togli(ActionEvent event) {
+
+	    }
+
+		@Override
+		public void init(Object parameters) {
+			// TODO Auto-generated method stub
+			
+		}
 
 }
