426a500bb8f78265dfc68d030ab290973dc36c44
gioripa <giorgio.ripamonti@studenti.unimi.it>
2022-03-04 17:46:12 +0100

    modifica

--- Voto_Elettronico/src/main/java/project/controller/InserisciCandidatoController.java
+++ Voto_Elettronico/src/main/java/project/controller/InserisciCandidatoController.java
@@ -1,9 +1,16 @@
 package project.controller;
 
+import dao.UtenteDAO;
+import factory.AlertFactory;
+import factory.DAOFactory;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import project.model.Elettore;
+import project.model.Scrutinatore;
+import project.model.Utente;
 
 public class InserisciCandidatoController extends Controller{
 
@@ -29,7 +36,6 @@ public class InserisciCandidatoController extends Controller{
 
     @FXML
     void inserisci(ActionEvent event) {
-
     }
 
 	@Override
